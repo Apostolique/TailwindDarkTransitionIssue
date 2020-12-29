971d5ed585cae928f3c437afe6acd5da4c4aaecb
Jean-David Moisan <vynecompiler@gmail.com>
2020-12-29 03:44:52 -0500

    Repro

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'media',
+  theme: {
+    extend: {
+      transitionDuration: {
+        '3000': '3000ms'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
